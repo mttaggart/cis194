1731adab017f73f74242a0585a9c5245de06249a
mttaggart <michael.t.taggart@gmail.com>
2016-11-19 15:59:31 -0800

    working hw1 1-4

--- hw1.hs
+++ hw1.hs
@@ -1,8 +1,18 @@
+import Data.Char
+
 toDigits :: Integer -> [Integer]
 toDigits n
   | n <= 0 = []
-  | otherwise = map (read . return) . show n
-
+  | otherwise = [fromIntegral (digitToInt x) | x <- show n]
 
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev n = reverse  toDigits n
+toDigitsRev n = reverse  (toDigits n)
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther = zipWith (*) (cycle [1,2])
+
+sumDigits :: [Integer] -> Integer
+sumDigits n = sum ((=<<) toDigits n)
+
+validate :: Integer -> Bool
+validate n = (sumDigits . doubleEveryOther $ toDigits n) `mod` 10 == 0
