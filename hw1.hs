4d9c71400428bb26f11da613c08791a39c5de702
mttaggart <michael.t.taggart@gmail.com>
2016-11-20 08:51:35 -0800

    working hw 1 1-5

--- hw1.hs
+++ hw1.hs
@@ -16,3 +16,15 @@ sumDigits n = sum ((=<<) toDigits n)
 
 validate :: Integer -> Bool
 validate n = (sumDigits . doubleEveryOther $ toDigits n) `mod` 10 == 0
+
+type Peg = String
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 1 a b c = [(a, b)]
+hanoi n a b c =
+  stepOneMoves ++ stepTwoMoves ++ stepThreeMoves
+  where
+    stepOneMoves = hanoi (n-1) a c b
+    stepTwoMoves = hanoi 1 a b c
+    stepThreeMoves = hanoi (n-1) c b a
